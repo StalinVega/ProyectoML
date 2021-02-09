6f9f3c912a4e0098bb1d0210f3a84d68d1bded99
StalinVega <stalinvega18@gmail.com>
2021-02-09 05:34:11 -0500

    primero

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+# EDA Packages
+import numpy as np
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(port=8080,debug=True)
